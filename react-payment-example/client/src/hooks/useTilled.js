5ec45de088ef26f5778344506f002ecdfba2fb77
Daniel Patton <65815343+dpatton1992@users.noreply.github.com>
2023-01-30 14:07:18 -0500

    fix: only inject fields if they dont have children

--- react-payment-example/client/src/hooks/useTilled.js
+++ react-payment-example/client/src/hooks/useTilled.js
@@ -6,15 +6,10 @@ import useScript from "./useScript";
 export default function useTilled(account_id, public_key, paymentTypeObj, fieldOptions) {
     // dynamically load tilled.js when component mounts
     const status = useScript("https://js.tilled.com/v2", "tilled-js-script");
-    const message = status === "error" ? "Tilled.js was unable to load." : `Tilled.js is ${status}.`
-
-    // initialize state
-    // selects the corresponding element using the first id in paymentTypeObj.fields and 
-    // uses it to determine whether or not there are any children (like tilled.js iframes)
-    let hasInitiated = document.getElementById(Object.values(paymentTypeObj.fields)[0].slice(1))?.childNodes.length > 0;
+    const message = status === "error" ? "Tilled.js was unable to load." : `Tilled.js is ${status}.`;
 
     async function initTilled() {
-        console.log("Initializing Tilled", paymentTypeObj)
+        console.log("Initializing Tilled", paymentTypeObj);
 
         // Create a new tilled instance
         paymentTypeObj.tilled = new window.Tilled(
@@ -24,21 +19,21 @@ export default function useTilled(account_id, public_key, paymentTypeObj, fieldO
                 sandbox: true,
                 log_level: 0
             }
-        )
+        );
 
         // await the form
         paymentTypeObj.form = await paymentTypeObj.tilled.form({
             payment_method_type: paymentTypeObj.type,
-        })
+        });
 
         // loop through fields and inject them
         Object.entries(paymentTypeObj.fields).forEach((entry) => {
             const [field, fieldId] = entry;
-            const fieldElement = document.getElementById(fieldId.slice(1))
+            const fieldElement = document.getElementById(fieldId.slice(1));
 
-            paymentTypeObj.form.createField(field, fieldOptions ? fieldOptions : {}).inject(fieldElement);
+            if (fieldElement.childElementCount === 0) paymentTypeObj.form.createField(field, fieldOptions ? fieldOptions : {}).inject(fieldElement);
         });
-
+        
         // update card brand
         if (paymentTypeObj.type === 'card' && document.getElementById('card-brand-icon')) {
             paymentTypeObj.form.fields.cardNumber.on('change', (evt) => {
@@ -62,25 +57,25 @@ export default function useTilled(account_id, public_key, paymentTypeObj, fieldO
             });
         }
         // Build the form
-        paymentTypeObj.form.build()
-        console.log("Tilled initialized", paymentTypeObj)
-    }
+        paymentTypeObj.form.build();
+        console.log("Tilled initialized", paymentTypeObj);
+    };
 
     function teardown() {
         if (paymentTypeObj.form) {
             paymentTypeObj.form.teardown((success) => {
                 paymentTypeObj.form = undefined;
-                console.log("The form teardown has run successfully:", success)
+                console.log("The form teardown has run successfully:", success);
             });
-        }
-    }
+        };
+    };
 
     useEffect(() => {
         // We could probably proxy the status, but this is simpler
-        if (status === 'ready' && !hasInitiated) initTilled()
+        if (status === 'ready') initTilled();
 
-        return teardown()
-    })
+        return teardown();
+    });
 
     return message;
 }
