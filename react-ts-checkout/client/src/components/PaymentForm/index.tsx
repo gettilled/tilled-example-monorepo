53324b06270b1c46345ce8ce9928c6292c555778
Daniel Patton <65815343+dpatton1992@users.noreply.github.com>
2023-05-09 09:52:56 -0400

    fix: handle returned PM on attach

--- react-ts-checkout/client/src/components/PaymentForm/index.tsx
+++ react-ts-checkout/client/src/components/PaymentForm/index.tsx
@@ -139,7 +139,15 @@ function PaymentForm(props: {
                     }),
                 }
             );
-            console.log(response);
+
+            if (response.status === 201) {
+                const pm = await response.json();
+                console.log(pm);
+                console.log('using saved pm', pm);
+                tilledParams = { payment_method: pm.id };
+            } else {
+                console.log(response);
+            }
         }
 
         if (paymentIntent) {
