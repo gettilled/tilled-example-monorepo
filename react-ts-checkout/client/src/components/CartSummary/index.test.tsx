3b3f74724b8f58691f6d8773eeebe39ec89e9b0c
Daniel Patton <65815343+dpatton1992@users.noreply.github.com>
2023-06-21 14:11:28 -0400

    feat: test that CartSummary renders

---
+++ react-ts-checkout/client/src/components/CartSummary/index.test.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import CartSummary from './index';
+
+import Shoes from './assets/shoes.jpg';
+import Socks from './assets/socks.jpg';
+import Gym from './assets/gym.jpg';
+import Lafleur from './assets/lafleur.webp';
+
+const cart = [
+    {
+        name: 'Running Shoes',
+        price: 9999,
+        imagePath: Shoes,
+        quantity: 1,
+    },
+    {
+        name: 'Socks',
+        price: 1999,
+        imagePath: Socks,
+        quantity: 2,
+    },
+    {
+        name: 'Gym Membership',
+        price: 3999,
+        imagePath: Gym,
+        quantity: 1,
+        subscription: {
+            billing_cycle_anchor: new Date(),
+            currency: 'usd',
+            interval_unit: 'month',
+            price: 3999,
+        },
+    },
+    {
+        name: 'Lafleur Package',
+        price: 1999,
+        imagePath: Lafleur,
+        quantity: 1,
+        subscription: {
+            billing_cycle_anchor: new Date(),
+            currency: 'usd',
+            interval_unit: 'month',
+            price: 999,
+        },
+    },
+];
+
+describe('CartSummary', () => {
+    it('should render the CartSummary component', () => {
+        render(<CartSummary cart={cart} />);
+        expect(screen.getByText('Cart Summary')).toBeInTheDocument();
+    });
+});
