1c7bfb29e2e1a3ed80180c6cb1e0cc4af37baf36
bdeklerk0 <109976127+bdeklerk0@users.noreply.github.com>
2023-10-03 14:50:34 -0500

    Feat/laravel php reporting (#70)

---
+++ laravel/laravel-php-reporting/app/Http/Controllers/ListPayoutsController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Client\RequestException;
+use App\Services\TilledService; // Import the TilledService class
+
+class ListPayoutsController extends Controller
+{
+    public function ListPayouts(Request $request)
+    {
+        $accountId = $request->header('tilled-account');
+        $offset = $request->query('offset') ?? 0;
+        try {
+            // Create an instance of TilledService by passing the account ID
+            $tilledService = new TilledService($accountId);
+            // Call the getTilledVariables method to retrieve the required variables
+            $tilledVariables = $tilledService->getTilledVariables();
+
+            // Retrieve the required values from the returned variables
+            $tilledSecretApiKey = $tilledVariables['tilledSecretApiKey'];
+            $baseUrl = $tilledVariables['tilledBaseUrl'];
+            $headers = [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer ' . $tilledSecretApiKey,
+                'tilled-Account' => $accountId,
+            ];
+            // Make the API request using a timeout to handle any potential delays
+            $response = Http::withHeaders($headers)->timeout(10)->get($baseUrl . '/v1/payouts?offset= ' . $offset . '&include=transaction_count' . '&limit=15');
+            // Check if the API request was successful
+            if ($response->successful()) {
+                $data = $response->json();
+                // Return the list of Payouts as a JSON response
+                return response()->json($data);
+            }
+            // If the request was not successful, handle errors
+            $errorMsg = 'Server error. Please try again later.';
+            $statusCode = $response->clientError() ? 400 : 500;
+            // Check if the request was a client error
+            if ($response->clientError()) {
+                $errorData = $response->json();
+                $errorMsg = $errorData['message'] ?? $errorMsg;
+            }
+            // Return the error message and status code as a JSON response
+            return response()->json(['message' => $errorMsg], $statusCode);
+        } catch (RequestException $exception) {
+            return response()->json(['message' => 'Unable to process the request. Please try again later.'], 500);
+        }
+    }
+}
