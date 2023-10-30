b987b0736501e9c82dc43ad8c712937f2cdfb7f8
Daniel Patton <65815343+dpatton1992@users.noreply.github.com>
2023-10-30 15:50:22 -0400

    Fix/cleanup react payment example (#79)

--- react/react-payment-example/client/src/App.jsx
+++ react/react-payment-example/client/src/App.jsx
@@ -1,131 +1,127 @@
-import { useState } from "react";
-import confirmPayment from "./functions/confirmPayment";
-import getSecret from "./functions/getSecret";
-import { useForm } from "react-hook-form";
-
-import CreditCardFields from "./components/credit-card-fields";
-import AchDebitFields from "./components/ach-debit-fields";
-import BillingDetailsFields from "./components/billing-details-fields";
-// import SavePaymentCheckbox from './components/save-payment-checkbox';
-import "./App.css";
-
-const pk_PUBLISHABLE_KEY = import.meta.env.VITE_TILLED_PUBLIC_KEY;
-const account_id = import.meta.env.VITE_TILLED_ACCOUNT_ID;
-
-const paymentMethodTypes = {
-  creditCard: {
-    type: "card",
-    fields: {
-      cardNumber: "#card-number-element",
-      cardExpiry: "#card-expiration-element",
-      cardCvv: "#card-cvv-element",
-    },
-  },
-  bankTransfer: {
-    type: "ach_debit",
-    fields: {
-      bankRoutingNumber: "#bank-routing-number-element",
-      bankAccountNumber: "#bank-account-number-element",
-    },
-  },
-};
-
-const navItems = [
-  {
-    id: 1,
-    title: "Credit Card",
-    iconClass: "nav-icon fa fa-credit-card",
-    content: (
-      <CreditCardFields
-        account_id={account_id}
-        public_key={pk_PUBLISHABLE_KEY}
-        paymentTypeObj={paymentMethodTypes.creditCard}
-      />
-    ),
-  },
-  {
-    id: 2,
-    title: "Bank Transfer",
-    iconClass: "nav-icon fa fa-university",
-    content: (
-      <AchDebitFields
-        account_id={account_id}
-        public_key={pk_PUBLISHABLE_KEY}
-        paymentTypeObj={paymentMethodTypes.bankTransfer}
-      />
-    ),
-  },
-];
+import { useState, useRef } from 'react';
+import confirmPayment from './functions/confirmPayment';
+import getSecret from './functions/getSecret';
+import { useForm } from 'react-hook-form';
+import CreditCardFields from './components/credit-card-fields';
+import AchDebitFields from './components/ach-debit-fields';
+import BillingDetailsFields from './components/billing-details-fields';
+import './App.css';
 
 function App() {
-  const [active, setActive] = useState(1);
+	const pk_PUBLISHABLE_KEY = import.meta.env.VITE_TILLED_PUBLIC_KEY;
+	const account_id = import.meta.env.VITE_TILLED_ACCOUNT_ID;
 
-  // const { handleSubmit, formState: { errors } } = useForm();
-  const { handleSubmit } = useForm();
-  // const onSubmit = data => console.log(data);
-  async function onSubmit(event) {
-    event.currentTarget.classList.add("opacity-50");
-    event.currentTarget.setAttribute("disabled", "");
-    const secret = await getSecret(account_id);
-    const thisType =
-      active === 1
-        ? paymentMethodTypes.creditCard
-        : paymentMethodTypes.bankTransfer;
+	const paymentMethodTypesRef = useRef({
+		card: {
+			type: 'card',
+			fields: {
+				cardNumber: '#card-number-element',
+				cardExpiry: '#card-expiration-element',
+				cardCvv: '#card-cvv-element',
+			},
+		},
+		ach_debit: {
+			type: 'ach_debit',
+			fields: {
+				bankRoutingNumber: '#bank-routing-number-element',
+				bankAccountNumber: '#bank-account-number-element',
+			},
+		},
+	});
 
-    await confirmPayment(thisType, secret);
-  }
+	const navItems = [
+		{
+			id: 1,
+			title: 'Credit Card',
+			iconClass: 'nav-icon fa fa-credit-card',
+			content: (
+				<CreditCardFields
+					account_id={account_id}
+					public_key={pk_PUBLISHABLE_KEY}
+					paymentTypeObj={paymentMethodTypesRef}
+				/>
+			),
+		},
+		{
+			id: 2,
+			title: 'Bank Transfer',
+			iconClass: 'nav-icon fa fa-university',
+			content: (
+				<AchDebitFields
+					account_id={account_id}
+					public_key={pk_PUBLISHABLE_KEY}
+					paymentTypeObj={paymentMethodTypesRef}
+				/>
+			),
+		},
+	];
 
-  return (
-    <div className="App checkout-app max-w-md p-5 bg-white shadow-lg">
-      <header className="App checkout-header">
-        <h1 className="text-3xl text-center mb-4">React Payment Example</h1>
-      </header>
-      <form className="credit-card_form" onSubmit={handleSubmit(onSubmit)}>
-        <ul className="nav flex justify-around m-3 rounded border">
-          {navItems.map(({ id, iconClass, title }) => (
-            <NavPill
-              key={title}
-              iconClass={iconClass}
-              title={title}
-              onItemClicked={() => setActive(id)}
-              isActive={active === id}
-            />
-          ))}
-        </ul>
-        <BillingDetailsFields />
-        <div className="content">
-          {navItems.map(({ id, content }) => {
-            return active === id ? content : "";
-          })}
-        </div>
-        {/* <SavePaymentCheckbox /> */}
-        <input
-          className="submit-btn w-full border rounded-md mt-6 p-3 h-auto bg-blue-700 text-xl text-white font-bold"
-          type="submit"
-          onClick={onSubmit}
-          value="Pay"
-        />
-      </form>
-    </div>
-  );
+	const [active, setActive] = useState(1);
+	const { handleSubmit } = useForm();
+	// const onSubmit = data => console.log(data);
+	async function onSubmit(event) {
+		event.currentTarget.classList.add('opacity-50');
+		event.currentTarget.setAttribute('disabled', '');
+		const secret = await getSecret(account_id);
+		const thisType =
+			active === 1
+				? paymentMethodTypesRef.current.card
+				: paymentMethodTypesRef.current.ach_debit;
+
+		await confirmPayment(thisType, secret);
+	}
+
+	return (
+		<div className="App checkout-app max-w-md p-5 bg-white shadow-lg">
+			<header className="App checkout-header">
+				<h1 className="text-3xl text-center mb-4">React Payment Example</h1>
+			</header>
+			<form className="credit-card_form" onSubmit={handleSubmit(onSubmit)}>
+				<ul className="nav flex justify-around m-3 rounded border">
+					{navItems.map(({ id, iconClass, title }) => (
+						<NavPill
+							key={title}
+							iconClass={iconClass}
+							title={title}
+							onItemClicked={() => setActive(id)}
+							isActive={active === id}
+						/>
+					))}
+				</ul>
+				<BillingDetailsFields />
+				<div className="content">
+					{navItems.map(({ id, content }) => {
+						return active === id ? content : '';
+					})}
+				</div>
+				{/* <SavePaymentCheckbox /> */}
+				<input
+					className="submit-btn w-full border rounded-md mt-6 p-3 h-auto bg-blue-700 text-xl text-white font-bold"
+					type="submit"
+					onClick={onSubmit}
+					value="Pay"
+				/>
+			</form>
+		</div>
+	);
 }
 
 const NavPill = ({
-  iconClass = "",
-  title = "",
-  onItemClicked = () =>
-    console.error("You didn't pass an action to the component"),
-  isActive = false,
+	iconClass = '',
+	title = '',
+	onItemClicked = () =>
+		console.error("You didn't pass an action to the component"),
+	isActive = false,
 }) => {
-  return (
-    <li
-      className={isActive ? "navItem" : "navItem navItem--inactive"}
-      onClick={onItemClicked}
-    >
-      <i className={iconClass} />
-      &nbsp;{title}
-    </li>
-  );
+	return (
+		<li
+			className={isActive ? 'navItem' : 'navItem navItem--inactive'}
+			onClick={onItemClicked}
+		>
+			<i className={iconClass} />
+			&nbsp;{title}
+		</li>
+	);
 };
 
 export default App;
--- react/react-payment-example/client/src/components/credit-card-fields.jsx
+++ react/react-payment-example/client/src/components/credit-card-fields.jsx
@@ -1,67 +1,76 @@
-import React from "react";
+import { useRef } from 'react';
 
-import { useForm, } from "react-hook-form";
-import useTilled from "../hooks/useTilled";
+import { useForm } from 'react-hook-form';
+import useTilled from '../hooks/useTilled';
 
 export default function CreditCardFields(props) {
-  const {
-    register,
-    formState: { errors },
-  } = useForm();
-  const fieldOptions = {
-    styles: {
-      base: {
-        fontFamily:
-          '-apple-system, "system-ui", "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
-        color: "#304166",
-        fontWeight: "400",
-        fontSize: "16px",
-      },
-      invalid: {
-        ":hover": {
-          textDecoration: "underline dotted red",
-        },
-        color: "#777777",
-      },
-      valid: {
-        color: "#32CD32",
-      },
-    },
-  };
+	const {
+		register,
+		formState: { errors },
+	} = useForm();
+	const fieldOptions = {
+		styles: {
+			base: {
+				fontFamily:
+					'-apple-system, "system-ui", "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
+				color: '#304166',
+				fontWeight: '400',
+				fontSize: '16px',
+			},
+			invalid: {
+				':hover': {
+					textDecoration: 'underline dotted red',
+				},
+				color: '#777777',
+			},
+			valid: {
+				color: '#32CD32',
+			},
+		},
+	};
 
-  const status = useTilled(props.account_id, props.public_key, props.paymentTypeObj, fieldOptions)
-  console.log(status)
+	const status = useTilled(
+		props.account_id,
+		props.public_key,
+		'card',
+		props.paymentTypeObj,
+		fieldOptions
+	);
+	console.log(status);
 
-  return (
-    <div>
-      <label className="flex flex-col p-3">
-        Card Number
-        <div className="tilled-js_input max-width-max border rounded-md flex justify-between">
-          <div id="card-number-element" {...register("cardNumber", { required: true, maxLength: 15 })} />
-          <div id="card-brand-icon" />
-        </div>
-        {errors.cardNumber?.type === "required" && "Card Number is required"}
-      </label>
-      <div className="grid grid-cols-2">
-        <label className="flex flex-col p-3">
-          Expiration
-          <div
-            className="tilled-js_input border rounded-md"
-            id="card-expiration-element"
-            {...register("expiration", { required: true })}
-          />
-          {errors.expiration?.type === "required" && "Expiration is required"}
-        </label>
-        <label className="flex flex-col p-3">
-          CVV
-          <div
-            className="tilled-js_input border rounded-md"
-            id="card-cvv-element"
-            {...register("cvv", { required: true, maxLength: 3 })}
-          />
-          {errors.cvv?.type === "required" && "cvv is required"}
-        </label>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<label className="flex flex-col p-3">
+				Card Number
+				<div className="tilled-js_input max-width-max border rounded-md flex justify-between">
+					<div
+						id="card-number-element"
+						{...register('cardNumber', { required: true, maxLength: 15 })}
+					/>
+					<div id="card-brand-icon" />
+				</div>
+				{errors.cardNumber?.type === 'required' && 'Card Number is required'}
+			</label>
+			<div className="grid grid-cols-2">
+				<label className="flex flex-col p-3">
+					Expiration
+					<div
+						className="tilled-js_input border rounded-md"
+						id="card-expiration-element"
+						{...register('expiration', { required: true })}
+					/>
+					{errors.expiration?.type === 'required' && 'Expiration is required'}
+				</label>
+				<label className="flex flex-col p-3">
+					CVV
+					<div
+						className="tilled-js_input border rounded-md"
+						id="card-cvv-element"
+						{...register('cvv', { required: true, maxLength: 3 })}
+					/>
+					{errors.cvv?.type === 'required' && 'cvv is required'}
+				</label>
+			</div>
+		</div>
+	);
 }
