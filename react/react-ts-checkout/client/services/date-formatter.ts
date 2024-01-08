ef14885888187db669d56820efd3d9a24b23c93d
Daniel Patton <65815343+dpatton1992@users.noreply.github.com>
2024-01-08 10:48:22 -0500

    feat: notify slack on new issue

--- react/react-ts-checkout/client/services/date-formatter.ts
+++
@@ -1,8 +0,0 @@
-export default function dateFormatter(dateString: string | number | Date) {
-	const formatter = new Intl.DateTimeFormat('en-US', {
-		dateStyle: 'medium',
-		timeStyle: 'medium',
-		timeZone: 'America/Denver',
-	});
-	return formatter.format(new Date(dateString));
-}
