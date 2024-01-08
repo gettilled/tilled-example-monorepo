6098692571506f26bffbe4e24481d6726f99497d
Daniel Patton <65815343+dpatton1992@users.noreply.github.com>
2024-01-08 10:54:03 -0500

    feat: notify slack on new issue (#95)

--- react/react-ts-checkout/client/services/currency-formatter.ts
+++
@@ -1,7 +0,0 @@
-export default function currencyFormatter(amount: number) {
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  });
-  return formatter.format(amount / 100)
-}
