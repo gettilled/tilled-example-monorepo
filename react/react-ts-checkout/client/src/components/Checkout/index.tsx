f47249861d97b7f163cabf01405b727183c61820
dpatton1992 <pattondt1992@gmail.com>
2024-05-29 10:02:44 -0400

    disable submit and allow option to create payment intent on submission

--- react/react-ts-checkout/client/src/components/Checkout/index.tsx
+++ react/react-ts-checkout/client/src/components/Checkout/index.tsx
@@ -1,118 +1,97 @@
-import { useRef } from 'react';
-import { useQuery } from 'react-query';
-import LoadingWheel from '../LoadingWheel';
-import Error from '../Error/Error';
-import PaymentForm from '../PaymentForm';
-import CartSummary from '../CartSummary';
-import { ThemeProvider } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
-import { fontStyle } from '@mui/system';
-import { request } from 'http';
+import { useQuery } from "react-query";
+import LoadingWheel from "../LoadingWheel";
+import Error from "../Error/Error";
+import PaymentForm from "../PaymentForm";
+import CartSummary from "../CartSummary";
+import { ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { fetchPaymentIntent } from "../PaymentForm/utils/fetchPaymentIntent";
+import { IPaymentIntent } from "../../models/PaymentIntents";
 
 const theme = createTheme({
-	palette: {
-		mode: 'light',
-		primary: {
-			main: '#334155',
-		},
-		secondary: {
-			main: '#01a2e9',
-		},
-	},
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#334155",
+    },
+    secondary: {
+      main: "#01a2e9",
+    },
+  },
 });
 
 type Subscription = {
-	billing_cycle_anchor: Date | string;
-	currency: string;
-	interval_unit: string;
-	price: number;
+  billing_cycle_anchor: Date | string;
+  currency: string;
+  interval_unit: string;
+  price: number;
 };
 
 export default function Checkout(props: {
-	cart: Array<{
-		name: string;
-		price: number;
-		imagePath: string;
-		quantity: number;
-		subscription?: Subscription;
-	}>;
+  cart: Array<{
+    name: string;
+    price: number;
+    imagePath: string;
+    quantity: number;
+    subscription?: Subscription;
+  }>;
 }) {
-	const tilledAccount = import.meta.env.VITE_TILLED_MERCHANT_ACCOUNT_ID;
-	const salesTax = Number(import.meta.env.VITE_TILLED_MERCHANT_TAX) || 1;
-	const cart = props.cart;
-	let loading = false;
-	let errored = false;
-	let errorObj = null;
-	let paymentIntent = null;
-	let subscriptions: Array<Subscription> = [];
+  const cart = props.cart;
+  let loading = false;
+  let errored = false;
+  let errorObj = null;
+  let paymentIntent: IPaymentIntent | undefined = undefined;
+  let subscriptions: Array<Subscription> = [];
 
-	cart.forEach((item) => {
-		if (item?.subscription) subscriptions.push(item.subscription);
-	});
+  cart.forEach((item) => {
+    if (item?.subscription) subscriptions.push(item.subscription);
+  });
 
-	const total = cart
-		.map((item) => (!item?.subscription ? item.price * item.quantity : 0)) // Convert the amounts to an array
-		.reduce((acc, num) => acc + num, 0); // Sum the array
+  const total = cart
+    .map((item) => (!item?.subscription ? item.price * item.quantity : 0)) // Convert the amounts to an array
+    .reduce((acc, num) => acc + num, 0); // Sum the array
 
-	const fetchPaymentIntent = async () => {
-		const requestHeaders: HeadersInit = new Headers();
-		requestHeaders.set('Content-Type', 'application/json');
-		if (tilledAccount) requestHeaders.set('tilled_account', tilledAccount);
+  // This is an ecommerce app, so we would create the payment intent as soon as the user adds
+  // an item to the cart. This allows us to record all attempted payments. This is just a
+  // mock example, so we'll create the payment intent when the user goes to the checkout page.
+  // Alternatively, we could create the payment intent when the user clicks the "Pay" button
+  // if we don't care about this data.
+  if (total > 0) {
+    const { isLoading, isError, error, data } =
+      useQuery(["paymentIntent"], () => fetchPaymentIntent(total), {
+        keepPreviousData: true,
+      });
+    loading = isLoading;
+    errored = isError;
+    errorObj = error;
+    paymentIntent = data;
+  }
 
-		const response = await fetch('/api/payment-intents', {
-			method: 'POST',
-			headers: requestHeaders,
-			body: JSON.stringify({
-				amount: Math.round(total * salesTax),
-				currency: 'usd',
-				payment_method_types: ['card', 'ach_debit'],
-			}),
-		});
+  // Uncomment for subscriptions only:
+  // const isLoading = false;
+  // const isError = false;
+  // const error = null;
 
-		if (!response.ok)
-			throw new (Error as any)(
-				`Unable to fetch payments from backend. Status: ${response.statusText}`
-			);
-
-		return response.json();
-	};
-
-	// Fetch payment intent from backend
-	if (total > 0) {
-		const { isLoading, isError, error, data, isFetching, isPreviousData } =
-			useQuery(['paymentIntent'], () => fetchPaymentIntent(), {
-				keepPreviousData: true,
-			});
-		loading = isLoading;
-		errored = isError;
-		errorObj = error;
-		paymentIntent = data;
-	}
-
-	// Uncomment for subscriptions:
-	// const isLoading = false;
-	// const isError = false;
-	// const error = null;
-
-	return loading ? (
-		<LoadingWheel />
-	) : (
-		<div className="container bg-white rounded-xl w-max shadow-md p-4 m-auto">
-			{errored ? (
-				<Error message={(errorObj as any).message} />
-			) : (
-				<div className="lg:grid lg:grid-cols-2 lg:divide-x divide-slate-400/25">
-					<CartSummary cart={cart} />
-					<ThemeProvider theme={theme}>
-						<PaymentForm
-							paymentIntent={paymentIntent}
-							subscriptions={subscriptions}
-						/>
-						{/* Uncomment to test subscriptions */}
-						{/* <PaymentForm recurring={true} /> */}
-					</ThemeProvider>
-				</div>
-			)}
-		</div>
-	);
+  return loading ? (
+    <LoadingWheel />
+  ) : (
+    <div className="container bg-white rounded-xl w-max shadow-md p-4 m-auto">
+{errored ? (
+	<Error message={(errorObj as any).message} />
+) : (
+	<div className="lg:grid lg:grid-cols-2 lg:divide-x divide-slate-400/25">
+		<CartSummary cart={cart} />
+		<ThemeProvider theme={theme}>
+			<PaymentForm
+				paymentIntent={paymentIntent}
+				amount={total}
+				subscriptions={subscriptions}
+			/>
+			{/* Uncomment to test subscriptions */}
+			{/* <PaymentForm recurring={true} /> */}
+		</ThemeProvider>
+	</div>
+)}
+    </div>
+  );
 }
---
+++ react/react-ts-checkout/client/src/components/PaymentForm/utils/fetchPaymentIntent.ts
@@ -0,0 +1,28 @@
+import { IPaymentIntent } from "../../../models/PaymentIntents";
+
+export const fetchPaymentIntent = async (amount: number): Promise<IPaymentIntent> => {
+	const tilledAccount = import.meta.env.VITE_TILLED_MERCHANT_ACCOUNT_ID;
+	const salesTax = Number(import.meta.env.VITE_TILLED_MERCHANT_TAX) || 1;
+    const requestHeaders: HeadersInit = new Headers();
+    requestHeaders.set('Content-Type', 'application/json');
+    if (tilledAccount) requestHeaders.set('tilled_account', tilledAccount);
+
+    const response = await fetch('/api/payment-intents', {
+        method: 'POST',
+        headers: requestHeaders,
+        body: JSON.stringify({
+            amount: Math.round(amount * salesTax),
+            currency: 'usd',
+            payment_method_types: ['card', 'ach_debit'],
+        }),
+    });
+
+    if (!response.ok)
+        throw new (Error as any)(
+            `Unable to fetch payments from backend. Status: ${response.statusText}`
+        );
+
+    return response.json();
+};
+
+export default fetchPaymentIntent;
