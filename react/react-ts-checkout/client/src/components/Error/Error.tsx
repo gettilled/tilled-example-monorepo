e5ad181e3801cdbc05559c05fbc494bce556c227
dpatton1992 <pattondt1992@gmail.com>
2025-01-14 09:52:55 -0500

    feat: prevent duplicate payments (#186)
    * deps updates
    
    * disable submit button after clicking it
    
    * warn for duplicate payments
    
    * fix: refactor to async await to prevent duplicate returns and use metadata id
    
    * Update error message
    
    * disable duplicate payment detection
    
    * update readme

--- react/react-ts-checkout/client/src/components/Error/Error.tsx
+++ react/react-ts-checkout/client/src/components/Error/Error.tsx
@@ -1,18 +1,20 @@
 import { faPersonFalling } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import {Button} from '@mui/material';
 
 interface ErrorProps {
     message?: string;
+    tryAgain?: () => void;
 }
 
 function Error(props: ErrorProps) {
-    const message = props.message;
+    const {message, tryAgain} = props;
     return (
         <div className='flex flex-col items-center p-2'>
             Sorry, there has been an error.
             <FontAwesomeIcon className='w-12' icon={faPersonFalling} />
             {message ? message : ''}
+            {tryAgain ? <Button onClick={tryAgain}>Try Again</Button> : ''}
         </div>
     );
 }
--- react/react-ts-checkout/client/src/components/PaymentForm/utils/fetchPaymentIntent.ts
+++ react/react-ts-checkout/client/src/components/PaymentForm/utils/fetchPaymentIntent.ts
@@ -1,28 +1,39 @@
 import { IPaymentIntent } from "../../../models/PaymentIntents";
 
-export const fetchPaymentIntent = async (amount: number): Promise<IPaymentIntent> => {
-	const tilledAccount = import.meta.env.VITE_TILLED_MERCHANT_ACCOUNT_ID;
-	const salesTax = Number(import.meta.env.VITE_TILLED_MERCHANT_TAX) || 1;
-    const requestHeaders: HeadersInit = new Headers();
-    requestHeaders.set('Content-Type', 'application/json');
-    if (tilledAccount) requestHeaders.set('tilled_account', tilledAccount);
+export const fetchPaymentIntent = async (
+  amount: number,
+  preventDuplicates = true
+): Promise<IPaymentIntent> => {
+  const tilledAccount = import.meta.env.VITE_TILLED_MERCHANT_ACCOUNT_ID;
+  const salesTax = Number(import.meta.env.VITE_TILLED_MERCHANT_TAX) || 1;
+  const requestHeaders: HeadersInit = new Headers();
+  requestHeaders.set("Content-Type", "application/json");
+  if (tilledAccount) requestHeaders.set("tilled_account", tilledAccount);
 
-    const response = await fetch('/api/payment-intents', {
-        method: 'POST',
-        headers: requestHeaders,
-        body: JSON.stringify({
-            amount: Math.round(amount * salesTax),
-            currency: 'usd',
-            payment_method_types: ['card', 'ach_debit'],
-        }),
-    });
+  const response = await fetch("/api/payment-intents", {
+    method: "POST",
+    headers: requestHeaders,
+    body: JSON.stringify({
+      payment_intent: {
+        amount: Math.round(amount * salesTax),
+        currency: "usd",
+        payment_method_types: ["card", "ach_debit"],
+        metadata: {
+          user_id: "user_12345",
+        },
+      },
+      prevent_duplicates: preventDuplicates,
+    }),
+  });
 
-    if (!response.ok)
-        throw new (Error as any)(
-            `Unable to fetch payments from backend. Status: ${response.statusText}`
-        );
-
-    return response.json();
+  if (!response.ok) {
+    const defaultErrorMessage = "Unable to fetch payments from backend.";
+    const responseBody = await response.json();
+    const { message = defaultErrorMessage } = responseBody || {};
+    
+    throw new (Error as any)(`${message} Status: ${response.statusText}`);
+  }
+  return response.json();
 };
 
 export default fetchPaymentIntent;
